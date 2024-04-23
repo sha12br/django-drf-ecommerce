3fb1a8679fa1f6108a5c6c4b2defc34b158aefb0
Shabeeruddin <shabeer@Shabeeruddins-MacBook-Pro.local>
2024-04-23 14:34:23 +0530

    built DRF router and viewset for Category and its serializer

--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .product import views
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
